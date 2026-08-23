1a89da5e744390a5a1dd53b67bd70b9ae2a3c4d2
thanveer-new <thanveernilambur@gmail.com>
2026-08-23 20:16:56 +0530

    Add outputs for EKS cluster and VPC details

---
+++ terraform/outputs.tf
@@ -0,0 +1,24 @@
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  description = "EKS cluster endpoint"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_arn" {
+  description = "EKS cluster ARN"
+  value       = module.eks.cluster_arn
+}
+
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
+}
+
+output "private_subnet_ids" {
+  description = "Private subnet IDs"
+  value       = module.vpc.private_subnets
+}
